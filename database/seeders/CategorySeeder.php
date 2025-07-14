d534fe057858040c101174f22011d4fb5ea63e47
Mengseu Sork <mengseu.sork212004@gmail.com>
2025-07-14 10:15:03 +0700

    fix code

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,18 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
     public function run()
     {
-        $categories = ['Tech', 'Health', 'Education', 'Travel'];
-        foreach ($categories as $name) {
-            Category::create(['name' => $name]);
-        }
+        Category::factory()->count(4)->create();
     }
 }
-
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,25 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Post;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
+use App\Models\Post;
 
 class PostSeeder extends Seeder
 {
     public function run()
     {
-        $faker = Faker::create();
-        $categoryIds = Category::pluck('id');
-
-        for ($i = 0; $i < 25; $i++) {
-            Post::create([
-                'title' => $faker->sentence,
-                'description' => $faker->paragraph,
-                'category_id' => $faker->randomElement($categoryIds),
-            ]);
-        }
+        Post::factory()->count(25)->create();
     }
 }
